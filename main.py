624eec7a22992a71ae3c6d854fe3820911909666
Paransaik <jty419@naver.com>
2021-11-21 22:37:05 +0900

    11.21. 코드 1차 수정

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ parser.add_argument('--video', help='Path to video file.',
                     default=os.path.basename("C:/Users/haram/PycharmProjects/OpenBankProject/"
                                              "tt.jpg"))
 '''
-
+'''
 # padding 된 폴더 경로
 filepath = "C:\\Users\\haram\\PycharmProjects\\OpenBankProject\\1024data\\2"
 # filepath = "C:\\Users\\haram\\PycharmProjects\\OpenBankProject\\1024data\\test"
@@ -56,147 +56,144 @@ for i, imagePath in enumerate(imagePaths):
     humans = estimator.inference(show)
     pose = TfPoseVisualizer.draw_pose_rgb(show, humans)
     cv.imwrite(save_filename, show)
+'''
+# model 1
+# smoke = load_model('./smoke/smoking_detector2.model')
+# model 2
+smoke = load_model('./smoke/smoking_add_skeleton_dataset_delete.model')
 
-# # model 1
-# # smoke = load_model('./smoke/smoking_detector2.model')
-# # model 2
-# smoke = load_model('./smoke/smoking_add_skeleton.model')
-#
-# # ArgumentParser에 원하는 description을 입력하여 parser객체 생성
-# parser = argparse.ArgumentParser(description='Action Recognition by OpenPose')  # openpose에 의한 작업 인식?
-#
-# # print('imagePath', imagePath)  # OpenBankProject/1024data/2/bb0695.jpg
-# save_dicname = imagePath.split("/")
-# # print('save_dicname', save_dicname)  # 'OpenBankProject', '1024data', '2', 'bb0695.jpg']
-# save_filename = "/".join(save_dicname[:-2]) + '/3/' + save_dicname[-1]
-# # print('save_filename', save_filename)  # OpenBankProject/1024data/3/bb0695.jpg
-#
-# parser.add_argument('--video', help='Path to video file.', default=imagePath)
-#
-# # parse_args() method로 명령창에서 주어진 인자를 파싱한다.
-# args = parser.parse_args()  # args 이름으로 파싱 성공시 args.parameter 형태로 주어진 인자 값을 받아서 사용가능
-#
-# # 관련 모델 가져오기
-# # tensorflow 추상화 라이브러리
-# estimator = load_pretrain_model('VGG_origin')  # 훈련 모델 로드(VGG_origin) 분류??
-# # print('estimator', estimator.graph_path)
-# # action파일 밑에 있는 Action/framewise_recognition.h5 모델 불러오기
-# action_classifier = load_action_premodel('Action/framewise_recognition.h5')
-#
-# # 인자 초기화
-# realtime_fps = '0.0000'
-# start_time = time.time()
-# fps_interval = 1
-# fps_count = 0
-# run_timer = 0
-# frame_count = 0
-#
-# # 동영상 파일 읽고 쓰기(웹캠 입력만 테스트)
-# cap = choose_run_mode(args)  # cap 객체에 choose_run_mode 파싱
-# # video_writer = set_video_writer(cap, write_fps=int(7.0))  # 비디오 fps설정
-#
-# # #관절을 저장하는 txt파일 (t)for training)
-# # f = open('origin_data.txt', 'a+')
-#
-# # model = load_model('./Model/smoking_detector2.model')  #, custom_objects={"InstanceNormalization": InstanceNormalization}
-#
-# while cv.waitKey(1) < 0: #키가 입력될때까지 반복
-#     data = []  #
-#     has_frame, show = cap.read()  # has_frame 과 show에 비디오를 한프레임씩 읽음 성공시 True, 실패시 False
-#     if has_frame:
-#         fps_count += 1  # fps 카운트
-#         frame_count += 1  # frame 카운트
-#
-#         # pose estimation 추정치 배열
-#         humans = estimator.inference(show)  # 비디오에서 사람 객체 추정
-#         # get pose info 사람의 동작을 추정 frame,joints, bboxes, xcenter 반환
-#         pose = TfPoseVisualizer.draw_pose_rgb(show, humans)  # return frame, joints, bboxes, xcenter
-#         for i in range(len(pose[5])):
-#             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#             s.connect((HOST, PORT))
-#             if ((pose[2][i][3] * 0.35) > (pose[5][i])) or ((pose[2][i][2] * 0.30) > (pose[6][i])):
-#                 msg = "1"  # exec(open("test4.py 1"))
-#                 s.send(msg.encode(encoding='utf_8', errors='strict'))
-#                 data = s.recv(1024)
-#                 print('result: ' + data.decode())
-#                 s.close()
-#             else:
-#                 msg = "0"  # exec(open("test4.py 0"))
-#                 s.send(msg.encode(encoding='utf_8', errors='strict'))
-#                 data = s.recv(1024)
-#                 print('result: ' + data.decode())
-#                 s.close()
-#
-#         image = img_to_array(show)  # image (450, 300, 3)
-#
-#         image = cv.resize(image, dsize=(224, 224),
-#                           interpolation=cv.INTER_LANCZOS4)  # lanczos 보간법  [[0.9569238  0.04307625]] [[0.9936041  0.00639589]]
-#
-#         # print("image resize",image.shape)
-#         image = preprocess_input(image)
-#         data.append(image)
-#
-#         data = np.array(data, dtype="float32")
-#         # print("data shape",data.shape)  # (1, 224, 224, 3)
-#         #
-#         # print("show shape",show.shape)  # (450, 300, 3)
-#         predIdxs = smoke.predict(data)
-#         print(predIdxs)
-#         predIdxs = np.argmax(predIdxs, axis=1)
-#         print(predIdxs)
-#         '''
-#         # 담배 인식 코드
-#         # 객체 수 많큼 반복
-#         pose[2][i] = [tl_x, tl_y, width, height]
-#         for i in range(len(pose[5])):
-#             # print(i, "h[5][i] = ", pose[5][i])  # 코과 손목 사이의 거리 비교, 높이를 사용
-#             # print(i, "h[2][i][3] * 0.35 = ", pose[2][i][3] * 0.35)  # 높이의 35% 값
-#             #
-#             # print(i, "w[6][i] = ", pose[6][i])  # 각각의 손목과 어깨 사이의 거리 비교, 너비 사용
-#             # print(i, "w[2][i][2] * 0.30 = ", pose[2][i][2] * 0.30)  # 너비의 30% 값
-#
-#             if ((pose[2][i][3] * 0.35) > (pose[5][i])) or ((pose[2][i][2] * 0.30) > (pose[6][i])):
-#                 print(1)
-#             else:
-#                 print(0)
-#         '''
-#         # recognize the action framewise
-#         # show = framewise_recognize(pose, action_classifier)
-#
-#         height, width = show.shape[:2] # 한 프레임씩 읽은 비디오의 높이, 넓이 크기
-#
-#         # 실시간 fps값 보이기
-#         if (time.time() - start_time) > fps_interval:
-#             # 이과정의 프레임 수 계산 INTERVAL이 1초라면 FPS
-#             realtime_fps = fps_count / (time.time() - start_time)
-#             fps_count = 0  # 프레임수 0
-#             start_time = time.time()
-#         fps_label = 'FPS:{0:.2f}'.format(realtime_fps)  # 출력 할 fps
-#         cv.putText(show, fps_label, (width-160, 25), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # 화면에 fps 출력
-#
-#         # 감지된 사람 수 보이기
-#         num_label = "Human: {0}".format(len(humans))  # 비디오에서 딴 사람객체 카운트해서 보여줌
-#         cv.putText(show, num_label, (5, height-45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # 화면에 사람 수 출력
-#
-#         # cv.putText(show, num_label, (5, height - 45), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)  # 화면에 사람 수 출력
-#
-#         # 현재 실행 시간 및 총 프레임 수 보이기
-#         if frame_count == 1:
-#             run_timer = time.time()
-#         run_time = time.time() - run_timer
-#         time_frame_label = '[Time:{0:.2f} | Frame:{1}]'.format(run_time, frame_count)
-#         cv.putText(show, time_frame_label, (5, height-15), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-#
-#         # cv.imshow('Action Recognition based on OpenPose', show)  # 창 이름
-#         # video_writer.write(show)  # 비디오 위에 쓰기
-#
-#         cv.imwrite(save_filename, show)
-#
-#         # 훈련용으로 데이터 수집 (for training)
-#         # joints_norm_per_frame = np.array(pose[-1]).astype(np.str)
-#         # f.write(' '.join(joints_norm_per_frame))
-#         # f.write('\n')
-#
-# # video_writer.release()  # 비디오 쓰기 해제
-# cap.release()  # cap 비디오 객체 해제
-# # f.close()
+# ArgumentParser에 원하는 description을 입력하여 parser객체 생성
+parser = argparse.ArgumentParser(description='Action Recognition by OpenPose')  # openpose에 의한 작업 인식?
+
+parser.add_argument('--video', help='Path to video file.',
+                    default=os.path.basename("C:/Users/haram/PycharmProjects/OpenBankProject/"
+                                             "tabaco1.mp4"))
+
+# parse_args() method로 명령창에서 주어진 인자를 파싱한다.
+args = parser.parse_args()  # args 이름으로 파싱 성공시 args.parameter 형태로 주어진 인자 값을 받아서 사용가능
+
+# 관련 모델 가져오기
+# tensorflow 추상화 라이브러리
+estimator = load_pretrain_model('VGG_origin')  # 훈련 모델 로드(VGG_origin) 분류??
+# print('estimator', estimator.graph_path)
+# action파일 밑에 있는 Action/framewise_recognition.h5 모델 불러오기
+action_classifier = load_action_premodel('Action/framewise_recognition.h5')
+
+# 인자 초기화
+realtime_fps = '0.0000'
+start_time = time.time()
+fps_interval = 1
+fps_count = 0
+run_timer = 0
+frame_count = 0
+
+# 동영상 파일 읽고 쓰기(웹캠 입력만 테스트)
+cap = choose_run_mode(args)  # cap 객체에 choose_run_mode 파싱
+# video_writer = set_video_writer(cap, write_fps=int(7.0))  # 비디오 fps설정
+
+# #관절을 저장하는 txt파일 (t)for training)
+# f = open('origin_data.txt', 'a+')
+
+# model = load_model('./Model/smoking_detector2.model')  #, custom_objects={"InstanceNormalization": InstanceNormalization}
+
+while cv.waitKey(1) < 0: #키가 입력될때까지 반복
+    data = []  #
+    has_frame, show = cap.read()  # has_frame 과 show에 비디오를 한프레임씩 읽음 성공시 True, 실패시 False
+    if has_frame:
+        fps_count += 1  # fps 카운트
+        frame_count += 1  # frame 카운트
+
+        # pose estimation 추정치 배열
+        humans = estimator.inference(show)  # 비디오에서 사람 객체 추정
+        # get pose info 사람의 동작을 추정 frame,joints, bboxes, xcenter 반환
+        pose = TfPoseVisualizer.draw_pose_rgb(show, humans)  # return frame, joints, bboxes, xcenter
+        for i in range(len(pose[5])):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            if ((pose[2][i][3] * 0.35) > (pose[5][i])) or ((pose[2][i][2] * 0.30) > (pose[6][i])):
+                msg = "1"  # exec(open("test4.py 1"))
+                s.send(msg.encode(encoding='utf_8', errors='strict'))
+                data = s.recv(1024)
+                print('result: ' + data.decode())
+                s.close()
+            else:
+                msg = "0"  # exec(open("test4.py 0"))
+                s.send(msg.encode(encoding='utf_8', errors='strict'))
+                data = s.recv(1024)
+                print('result: ' + data.decode())
+                s.close()
+
+        image = img_to_array(show)  # image (450, 300, 3)
+
+        image = cv.resize(image, dsize=(224, 224),
+                          interpolation=cv.INTER_LANCZOS4)  # lanczos 보간법  [[0.9569238  0.04307625]] [[0.9936041  0.00639589]]
+
+        # print("image resize",image.shape)
+        image = preprocess_input(image)
+        data.append(image)
+
+        data = np.array(data, dtype="float32")
+        # print("data shape",data.shape)  # (1, 224, 224, 3)
+        #
+        # print("show shape",show.shape)  # (450, 300, 3)
+        predIdxs = smoke.predict(data)
+        print(predIdxs)
+        predIdxs = np.argmax(predIdxs, axis=1)
+        print(predIdxs)
+        '''
+        # 담배 인식 코드
+        # 객체 수 많큼 반복
+        pose[2][i] = [tl_x, tl_y, width, height]
+        for i in range(len(pose[5])):
+            # print(i, "h[5][i] = ", pose[5][i])  # 코과 손목 사이의 거리 비교, 높이를 사용
+            # print(i, "h[2][i][3] * 0.35 = ", pose[2][i][3] * 0.35)  # 높이의 35% 값
+            #
+            # print(i, "w[6][i] = ", pose[6][i])  # 각각의 손목과 어깨 사이의 거리 비교, 너비 사용
+            # print(i, "w[2][i][2] * 0.30 = ", pose[2][i][2] * 0.30)  # 너비의 30% 값
+
+            if ((pose[2][i][3] * 0.35) > (pose[5][i])) or ((pose[2][i][2] * 0.30) > (pose[6][i])):
+                print(1)
+            else:
+                print(0)
+        '''
+        # recognize the action framewise
+        # show = framewise_recognize(pose, action_classifier)
+
+        height, width = show.shape[:2] # 한 프레임씩 읽은 비디오의 높이, 넓이 크기
+
+        # 실시간 fps값 보이기
+        if (time.time() - start_time) > fps_interval:
+            # 이과정의 프레임 수 계산 INTERVAL이 1초라면 FPS
+            realtime_fps = fps_count / (time.time() - start_time)
+            fps_count = 0  # 프레임수 0
+            start_time = time.time()
+        fps_label = 'FPS:{0:.2f}'.format(realtime_fps)  # 출력 할 fps
+        cv.putText(show, fps_label, (width-160, 25), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # 화면에 fps 출력
+
+        # 감지된 사람 수 보이기
+        num_label = "Human: {0}".format(len(humans))  # 비디오에서 딴 사람객체 카운트해서 보여줌
+        cv.putText(show, num_label, (5, height-45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)  # 화면에 사람 수 출력
+
+        # cv.putText(show, num_label, (5, height - 45), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)  # 화면에 사람 수 출력
+
+        # 현재 실행 시간 및 총 프레임 수 보이기
+        if frame_count == 1:
+            run_timer = time.time()
+        run_time = time.time() - run_timer
+        time_frame_label = '[Time:{0:.2f} | Frame:{1}]'.format(run_time, frame_count)
+        cv.putText(show, time_frame_label, (5, height-15), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+        cv.imshow('Action Recognition based on OpenPose', show)  # 창 이름
+        # video_writer.write(show)  # 비디오 위에 쓰기
+
+        # Image Save
+        # cv.imwrite(save_filename, show)
+
+        # 훈련용으로 데이터 수집 (for training)
+        # joints_norm_per_frame = np.array(pose[-1]).astype(np.str)
+        # f.write(' '.join(joints_norm_per_frame))
+        # f.write('\n')
+
+# video_writer.release()  # 비디오 쓰기 해제
+cap.release()  # cap 비디오 객체 해제
+# f.close()
